ffa17aba56b0f6020b30fabe4859d0f037930055
Agustin Monetti <agmonetti@uade.edu.ar>
2026-03-11 17:11:05 -0300

    mejora la función de iteración de punto fijo con mensajes de impresión más claros y elimina impresiones innecesarias

--- puntoFijo.py
+++ puntoFijo.py
@@ -10,10 +10,12 @@ def fixed_point_iteration(x0, tol=1e-5, max_iter=100):
     for i in range(max_iter):
         x_new = g(x)
         iter_values.append(x_new)
-        print(x_new)
+        print("Iteracion " + str(i+1) + ": " + str(x_new))
 
         if abs(x_new - x) < tol:
-            print("Tolerance exceeded...")
+            print("Tolerance exceeded..." )
+            print(f"Converged to {x_new} after {i+1} iterations.")
+            print(f"final error: {abs(x_new - x)}")
             break
 
         x = x_new
@@ -22,6 +24,3 @@ def fixed_point_iteration(x0, tol=1e-5, max_iter=100):
 
 x0 = 1.5
 root, iter_values = fixed_point_iteration(x0)
-
-print("raiz:", root)
-print("Iteraciones:", len(iter_values)-1)
