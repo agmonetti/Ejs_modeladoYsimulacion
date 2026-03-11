ad88ea1c02073320f6887780c60e361fdc939595
Agustin Monetti <agmonetti@uade.edu.ar>
2026-03-11 17:06:44 -0300

    actualiza la función g para usar una nueva fórmula y mejora los mensajes de impresión en la iteración

--- puntoFijo.py
+++ puntoFijo.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def g(x):
-    return np.exp(-x)
+    return np.pi + 0.5 * np.sin(x / 2)
 
 def fixed_point_iteration(x0, tol=1e-5, max_iter=100):
     x = x0
@@ -10,16 +10,18 @@ def fixed_point_iteration(x0, tol=1e-5, max_iter=100):
     for i in range(max_iter):
         x_new = g(x)
         iter_values.append(x_new)
+        print(x_new)
 
         if abs(x_new - x) < tol:
-            print("Tolerance reached")
+            print("Tolerance exceeded...")
             break
 
         x = x_new
 
     return x_new, iter_values
 
-x0 = 0.5
+x0 = 1.5
 root, iter_values = fixed_point_iteration(x0)
 
 print("raiz:", root)
+print("Iteraciones:", len(iter_values)-1)
